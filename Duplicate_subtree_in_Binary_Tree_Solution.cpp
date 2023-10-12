8b35e16708a7fa4b21b6a86c26b01263ab9c867d
Anurag Singh <131255604+Untolders@users.noreply.github.com>
2023-10-12 20:37:05 +0530

    Create Duplicate_subtree_in_Binary_Tree_Solution.cpp

---
+++ Duplicate_subtree_in_Binary_Tree_Solution.cpp
@@ -0,0 +1,180 @@
+***********************************************************************************  Question *************************************************************************************************************************************************
+Given a binary tree, find out whether it contains a duplicate sub-tree of size two or more, or not.
+
+Note: Two same leaf nodes are not considered as subtree as size of a leaf node is one. 
+
+Example 1 :
+
+Input : 
+               1
+             /   \ 
+           2       3
+         /   \       \    
+        4     5       2     
+                     /  \    
+                    4    5
+Output : 1
+Explanation : 
+    2     
+  /   \    
+ 4     5
+is the duplicate sub-tree.
+Example 2 :
+
+Input : 
+               1
+             /   \ 
+           2       3
+Output: 0
+Explanation: There is no duplicate sub-tree 
+in the given binary tree.
+Your Task:  
+You don't need to read input or print anything. Your task is to complete the function dupSub() which takes root of the tree as the only argument and returns 1 if the binary tree contains a duplicate sub-tree of size two or more, else 0.
+
+Expected Time Complexity: O(N)
+Expected Space Complexity: O(N)
+
+Constraints:
+0 ≤ Data of nodes ≤ 9
+1 ≤ Number of nodes ≤ 105 
+
+
+
+
+************************************************************************************  Solution  ************************************************************************************************************************************************
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+	char data;
+	struct Node *left;
+	struct Node *right;
+
+	Node(char x) {
+		data = x;
+		left = NULL;
+		right = NULL;
+	}
+};
+
+struct Node* buildTree(string str)
+{
+	// Corner Case
+	if (str.length() == 0 || str[0] == 'N')
+		return NULL;
+
+	// Creating vector of strings from input
+	// string after spliting by space
+	vector<string> ip;
+
+	istringstream iss(str);
+	for (string str; iss >> str; )
+		ip.push_back(str);
+
+	// Create the root of the tree
+	Node *root = new Node(stoi(ip[0]));
+
+	// Push the root to the queue
+	queue<Node*> queue;
+	queue.push(root);
+
+	// Starting from the second element
+	int i = 1;
+	while (!queue.empty() && i < ip.size()) {
+
+		// Get and remove the front of the queue
+		Node* currNode = queue.front();
+		queue.pop();
+
+		// Get the current node's value from the string
+		string currVal = ip[i];
+
+		// If the left child is not null
+		if (currVal != "N") {
+
+			// Create the left child for the current Node
+			currNode->left = new Node(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->left);
+		}
+
+		// For the right child
+		i++;
+		if (i >= ip.size())
+			break;
+		currVal = ip[i];
+
+		// If the right child is not null
+		if (currVal != "N") {
+
+			// Create the right child for the current node
+			currNode->right = new Node(stoi(currVal));
+
+			// Push it to the queue
+			queue.push(currNode->right);
+		}
+		i++;
+	}
+
+	return root;
+}
+
+
+// } Driver Code Ends
+/*The structure of the Binary Tree Node  is
+struct Node
+{
+  char data;
+  struct Node* left;
+  struct Node* right;
+};*/
+
+class Solution {
+  public:
+    map<string, int> mpp;
+
+    string solve(Node* root) {
+        if (root == NULL) return "N";
+        string s="";
+        if(root->left==NULL && root->right==NULL){
+            s+=to_string(root->data);
+            return s;
+        }
+        s+=to_string(root->data)+solve(root->left)+solve(root->right);
+        mpp[s]++;
+        return s;
+    }
+    
+    int dupSub(Node* root) {
+        mpp.clear();
+        solve(root);
+        for(auto &it:mpp){
+            if(it.second>=2) return 1;
+        }
+        return 0;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	
+	int t;
+	cin >> t;
+	//cout << t << "\n";
+	while (t--)
+	{
+		string treeString;
+		getline(cin >> ws, treeString);
+		struct Node* root = buildTree(treeString);
+		Solution ob;
+		cout << ob.dupSub(root) << "\n";
+	}
+	return 0;
+}
+// } Driver Code Ends
+
